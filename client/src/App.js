818f6b14adbca911855972aca7e70c46e23342af
Alen Jo <alenjo874@gmail.com>
2022-04-04 17:06:05 -0400

    setting up scss style sheets

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import MainPage from "./components/Main/MainPage";
+import "./style/style.css";
 
 function App() {
   return (
     <div className="App">
-    
+      <MainPage />
     </div>
   );
 }
