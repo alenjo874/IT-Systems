00fa6d11dc1a2aeaf821fc95eb25c5ed84e169a9
Alen Jo <alenjo874@gmail.com>
2022-04-04 17:57:31 -0400

    database deployment check

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
+import React, { useEffect, useState } from "react";
 import MainPage from "./components/Main/MainPage";
 import "./style/style.css";
 
 function App() {
+  const [ticketsArray, setTicketsArray] = useState([]);
+
+  useEffect(() => {
+    fetch("/tickets")
+      .then((res) => res.json())
+      .then(setTicketsArray);
+  }, []);
+
+  const displayTickets = ticketsArray.map((ticket) => {
+    return <p>{ticket.subject}</p>;
+  });
+
   return (
     <div className="App">
       <MainPage />
+      {displayTickets}
     </div>
   );
 }
