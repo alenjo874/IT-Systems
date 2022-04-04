5caa06c1e6eb6887f88f7464d481b4defc54d1d5
Alen Jo <alenjo874@gmail.com>
2022-04-04 17:47:08 -0400

    added error handleing for routes

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,2 +1,35 @@
 class AdminsController < ApplicationController
+    def index
+        admins = Admin.all
+        render json: admins, status: :ok
+    end
+
+    def show
+        show_admin = Admin.find_by!(id: params[:id])
+        render json: show_admin, status: :ok
+    end
+
+    def create 
+        create_admin = Admin.create!(admin_params)
+        render json: create_admin, status: :created
+    end
+
+    def update 
+        update_admin = Admin.find_by!(id: params[:id])
+        update_admin.update(admin_params)
+        render json: update_admin, status: :accpeted
+    end
+
+    def destroy
+        destroy_admin = Admin.find_by!(id: params[:id])
+        destroy_admin.destroy
+        render json: {}, status: :accepted
+    end
+
+    private
+
+    def admin_params
+        params.permit(:name)
+    end
+
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  rescue_from ActiveRecord::RecordInvalid, with: :handle_invalid
+  rescue_from ActiveRecord::RecordNotFound, with: :handle_not_found
+
+  private
+
+  def handle_invalid(invalid)
+    render json: {errors: invalid.record.errors}, status: :unprocessable_entity
+  end  
+
+  def handle_not_found(exception)
+    render json: {errors: exception}, status: :not_found
+  end
+
 end
