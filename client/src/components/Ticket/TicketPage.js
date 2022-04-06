ebc308a9088cd01731742eec94fd6432c4573802
Alen Jo <alenjo874@gmail.com>
2022-04-06 12:40:41 -0400

    remove comet chat

--- client/src/components/Ticket/TicketPage.js
+++ client/src/components/Ticket/TicketPage.js
@@ -3,8 +3,7 @@ import { v4 as uuidv4 } from "uuid";
 import TicketCard from "./TicketCard";
 import TicketResolve from "./TicketResolve";
 import TicketUpcoming from "./TicketUpcoming";
-import { CometChat } from "@cometchat-pro/chat";
-import { CometChatUI } from "../../cometchat-pro-react-ui-kit/CometChatWorkspace/src";
+
 
 function TicketPage() {
   const [ticketsArray, setTicketsArray] = useState([]);
@@ -64,18 +63,6 @@ function TicketPage() {
 
   // ============================================================
 
-
-  const authKey = "d82fc71a268113db7b4e85aa8a9ccccd534d4ac5";
-  const uid = "it-admin";
-  CometChat.login(uid, authKey).then(
-    (user) => {
-      console.log("Login Successful:", { user });
-    },
-    (error) => {
-      console.log("Login failed with exception:", { error });
-    }
-  );
-
   return (
     <div className="ticket-page-container">
       <div className="ticket-card-container">
@@ -100,9 +87,6 @@ function TicketPage() {
       </div>
 
       <div>{displayCompleteTickets}</div>
-      <div style={{ width: "800px", height: "800px" }}>
-        <CometChatUI />
-      </div>
     </div>
   );
 }
