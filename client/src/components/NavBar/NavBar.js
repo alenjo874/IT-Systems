7c14b2df79360a0f5475045316d057ad8cd02c45
Alen Jo <alenjo874@gmail.com>
2022-04-07 16:12:54 -0400

    nav side bar

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -15,20 +15,6 @@ function NavBar() {
   const moreNav = (
     <motion.div
       className="sidebar-nav-container"
-      initial={{ opacity: 0 }}
-      animate={{
-        opacity: 1,
-        transition: {
-          duration: 0.25,
-          type: "show",
-          ease: "easeIn",
-        },
-      }}
-      exit={{
-        x: "-10%",
-        opacity: 0,
-        transition: { duration: 0.25, ease: "easeOut" },
-      }}
     >
       <Link to="/" className="nav-elem">
         Main
@@ -54,11 +40,7 @@ function NavBar() {
         {moreNav}
       </div> */}
 
-      <div className="sidebar-nav-container">
-        <div className="nav-icons">
-          <img src={search} />
-        </div>
-      </div>
+  {moreNav}
     </div>
   );
 }
