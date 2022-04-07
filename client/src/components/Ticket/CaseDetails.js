97e00a705392e9e62ae4967f39a84b82a8de3aaa
Alen Jo <alenjo874@gmail.com>
2022-04-07 14:03:52 -0400

    ticket card 2col layout

--- client/src/components/Ticket/CaseDetails.js
+++ client/src/components/Ticket/CaseDetails.js
@@ -3,27 +3,45 @@ import React from "react";
 function CaseDetails({ employee }) {
   return (
     <div className="case-details card">
+      <div className="shared-subsection">
+        <div className="ticket-subsections two-col">
+          <div>
+            <p className="detail-label">Contact Name</p>
+            <p> {employee ? employee.name : null} </p>
+          </div>
+        </div>
+        <div className="ticket-subsections two-col">
+          <div>
+            <p className="detail-label">Escalate</p>
+            <p> {employee ? employee.name : null} </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="shared-subsection">
+        <div className="ticket-subsections two-col">
+          <div>
+            <p className="detail-label">Department</p>
+            <p> {employee ? employee.name : null} </p>
+          </div>
+        </div>
+        <div className="ticket-subsections two-col">
+          <div>
+            <p className="detail-label">Position</p>
+            <p> {employee ? employee.name : null} </p>
+          </div>
+        </div>
+      </div>
+
       <div className="ticket-subsections">
         <div>
-          <p className="detail-label">Case Number</p>
+          <p className="detail-label">Email</p>
           <p> {employee ? employee.name : null} </p>
         </div>
       </div>
       <div className="ticket-subsections">
         <div>
-          <p className="detail-label">Contact Name</p>
-          <p> {employee ? employee.name : null} </p>
-        </div>
-      </div>
-      <div className="ticket-subsections">
-        <div>
-          <p className="detail-label">Escalate</p>
-          <p> {employee ? employee.name : null} </p>
-        </div>
-      </div>
-      <div className="ticket-subsections">
-        <div>
-          <p className="detail-label">Status</p>
+          <p className="detail-label">Mobile</p>
           <p> {employee ? employee.name : null} </p>
         </div>
       </div>
--- client/src/components/Ticket/TicketDetails.js
+++ client/src/components/Ticket/TicketDetails.js
@@ -7,29 +7,53 @@ function TicketDetails({
   level,
   employee,
   rental_item,
+  complete
 }) {
   return (
     <div className="ticket-details card">
+      <div className="shared-subsection">
+        <div className="ticket-subsections two-col">
+          <div>
+            <p className="detail-label">Case Number</p>
+            <p> {employee ? employee.name : null} </p>
+          </div>
+        </div>
+        <div className="ticket-subsections two-col">
+          <div>
+            <p className="detail-label">Case Priority</p>
+            <p
+              className={
+                level === "Low"
+                  ? "green"
+                  : level === "Moderate"
+                  ? "yellow"
+                  : "red"
+              }
+            >
+              {level}
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="shared-subsection">
+        <div className="ticket-subsections two-col">
+          <div>
+            <p className="detail-label">Case Date</p>
+            <p> {create_date} </p>
+          </div>
+        </div>
+        <div className="ticket-subsections two-col">
+          <div>
+            <p className="detail-label">Case Status</p>
+            <p> {!complete ? "New" : null} </p>
+          </div>
+        </div>
+      </div>
+
       <div className="ticket-subsections">
         <div>
           <p className="detail-label">Subject</p>
           <p> {subject} </p>
-        </div>
-      </div>
-      <div className="ticket-subsections">
-        <div>
-          <p className="detail-label">Case Priority</p>
-          <p
-            className={
-              level === "Low"
-                ? "green"
-                : level === "Moderate"
-                ? "yellow"
-                : "red"
-            }
-          >
-            {level}
-          </p>
         </div>
       </div>
       <div className="ticket-subsections">
@@ -42,12 +66,6 @@ function TicketDetails({
         <div>
           <p className="detail-label">Product</p>
           <p> {rental_item ? rental_item.name : null} </p>
-        </div>
-      </div>
-      <div className="ticket-subsections">
-        <div>
-          <p className="detail-label">Case Date</p>
-          <p> {create_date} </p>
         </div>
       </div>
     </div>
