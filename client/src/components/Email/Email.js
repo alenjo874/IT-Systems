1056874cf09bd4077dda6f0b881253a8649c4871
Alen Jo <alenjo874@gmail.com>
2022-04-11 14:36:41 -0400

    email feature added

---
+++ client/src/components/Email/Email.js
@@ -0,0 +1,108 @@
+import React, { useState } from "react";
+import { send } from "emailjs-com";
+import { motion, AnimatePresence } from "framer-motion/dist/framer-motion";
+
+function Email() {
+  const [toSend, setToSend] = useState({
+    from_name: "",
+    message: "",
+    reply_to: "",
+  });
+  const [confirmEmailSent, setConfirmEmailSent] = useState(false);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    send("service_3yu9ko8", "template_hbs9bjg", toSend, "88Gf9d8y15pb9xkOr")
+      .then((response) => {})
+      .catch((err) => {});
+
+    setToSend({
+      from_name: "",
+      message: "",
+      reply_to: "",
+    });
+    setConfirmEmailSent(true);
+    setTimeout(() => {
+      setConfirmEmailSent(false);
+    }, 2500);
+  };
+
+  const handleChange = (e) => {
+    setToSend({ ...toSend, [e.target.name]: e.target.value });
+  };
+  return (
+    <div className="pages-container" id="contact">
+      <div className="pages-header-container">
+        <h2 className="pages-name">Contact Me</h2>
+        <div className="header-backdrop"></div>
+      </div>
+      <div className="call-to-action">
+        <p>
+          Please send what your issue is, and an IT admin will assist shortly.
+        </p>
+      </div>
+      <form className="email-form" onSubmit={onSubmit}>
+        <input
+          type="text"
+          name="from_name"
+          placeholder="Your Name"
+          value={toSend.from_name}
+          onChange={handleChange}
+        />
+
+        <input
+          type="text"
+          name="reply_to"
+          placeholder="Your Email"
+          value={toSend.reply_to}
+          onChange={handleChange}
+        />
+        <textarea
+          type="text"
+          name="message"
+          placeholder="Your message"
+          value={toSend.message}
+          onChange={handleChange}
+        />
+        <button type="submit" className="view-work-btn">
+          Submit
+        </button>
+      </form>
+      <AnimatePresence>
+        {confirmEmailSent ? (
+          <div className="email-confirm-container">
+            <motion.div
+              className="email-popup"
+              initial={{ opacity: 0 }}
+              animate={{
+                opacity: 1,
+                transition: {
+                  duration: 0.25,
+                  type: "show",
+                  ease: "easeIn",
+                },
+              }}
+              exit={{
+                y: "10%",
+                opacity: 0,
+                transition: { duration: 0.25, ease: "easeOut" },
+              }}
+            >
+              <p>Email sent, thank you for your time!</p>
+              <div>
+                <button
+                  onClick={(e) => setConfirmEmailSent(false)}
+                  className="hide-button"
+                >
+                  X
+                </button>
+              </div>
+            </motion.div>
+          </div>
+        ) : null}
+      </AnimatePresence>
+    </div>
+  );
+}
+
+export default Email;
--- client/src/components/Employee/EmployeeEmail.js
+++ client/src/components/Employee/EmployeeEmail.js
@@ -1,17 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import Chat from "../Chat/Chat";
 import ChatSignIn from "../Chat/ChatSignIn";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../Chat/firebase";
 import ChatSignOut from "../Chat/ChatSignOut";
+import Email from "../Email/Email";
 
 function EmployeeEmail() {
   const [user] = useAuthState(auth);
+  const [toggleState, setToggleState] = useState(1);
+
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
 
   return (
     <div className="emp-email-container">
-      {user ? <Chat /> : <ChatSignIn />}
-      <ChatSignOut />
+      <div className="ticket-tab-container">
+        <div className="bloc-tabs">
+          <button
+            className={toggleState === 1 ? "tabs active-tabs" : "tabs"}
+            onClick={() => toggleTab(1)}
+          >
+            Ticket Feed
+          </button>
+          <button
+            className={toggleState === 2 ? "tabs active-tabs" : "tabs"}
+            onClick={() => toggleTab(2)}
+          >
+            Email
+          </button>
+          <button
+            className={toggleState === 3 ? "tabs active-tabs" : "tabs"}
+            onClick={() => toggleTab(3)}
+          >
+            Chat
+          </button>
+        </div>
+
+        <div className="content-tabs">
+          <div
+            className={
+              toggleState === 1 ? "content  active-content" : "content"
+            }
+          >
+            <div className="display-ticket card"> </div>
+          </div>
+
+          <div
+            className={
+              toggleState === 2 ? "content  active-content" : "content"
+            }
+          >
+            <Email />
+          </div>
+
+          <div
+            className={
+              toggleState === 3 ? "content  active-content" : "content"
+            }
+          >
+            {user ? <Chat /> : <ChatSignIn />}
+            <ChatSignOut />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
